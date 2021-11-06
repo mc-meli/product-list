6ec74595817869b23dd4c6777daefa2fbf54dcf6
mc-meli <93822072+mc-meli@users.noreply.github.com>
2021-11-06 08:17:04 -0300

    Created Xcode project

---
+++ ProductList/ProductListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListApp.swift
+//  ProductList
+//
+//  Created by Test on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ProductListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
