7407f8cbd32886907bb13a80569117002744b6e7
mc-meli <93822072+mc-meli@users.noreply.github.com>
2021-11-06 10:39:42 -0300

    Added product detail view
    Also: reordered view classes

---
+++ ProductList/Views/ProductDetailView.swift
@@ -0,0 +1,48 @@
+//
+//  ProductDetailView.swift
+//  ProductList
+//
+//  Created by Test on 6/11/21.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var product: ProductInfo
+    
+    @State private var showingAlert = false
+    
+    var body: some View {
+        VStack {
+            
+            HStack(alignment: .top) {
+                ProductImageView(withURL: product.thumbnail.absoluteString)
+                    .frame(width: 80)
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(product.title)
+                        .font(.title2)
+                    Text("Condición: \(product.condition == "used" ? "Usado" : "Nuevo")")
+                    Text("Precio: \(product.currency) \(product.price)")
+                }
+                
+            }
+            .padding()
+            
+            Button("Lo quiero!") {
+                showingAlert = true
+            }
+            .alert(isPresented: $showingAlert) {
+                Alert(
+                    title: Text("¡Gracias por tu compra!")
+                )
+            }
+            .padding()
+        }
+    }
+}
+
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView(product: previewProductInfo)
+    }
+}
